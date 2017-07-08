9446ae4a5e0a4a71b77bd3d2fe2f385c92faf388
ganeshp <ganesh_pachpind@external.mckinsey.com>
2017-07-08 16:01:49 +0530

    Initial project setup

---
+++ src/main/java/com/goeuro/BusroutesApplication.java
@@ -0,0 +1,12 @@
+package com.goeuro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusroutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusroutesApplication.class, args);
+	}
+}
